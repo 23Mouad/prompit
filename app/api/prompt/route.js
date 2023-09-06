6936640c287bd0488b85dcebaaeec87bd76b8e11
MOUAD <mouadmire8@gmail.com>
2023-09-06 20:59:49 +0100

    fix rendering problem

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,3 +1,7 @@
+import { connectDB } from "@utils/database";
+import Prompt from "@models/Prompt";
+import User from "@models/User";
+
 export const revalidate = 1; //revalidate api every 1 second
 export const GET = async (request) => {
    try {
